bb775f44a1159a0fd9dc2943cf580a071252761e
carolynduby <carolynduby@ieee.org>
2015-07-23 19:51:55 -0400

    Delete deploy.R

--- shiny/deploy.R
+++
@@ -1,3 +0,0 @@
-library(shinyapps)
-shinyapps::setAccountInfo(name='cduby-dscience', token='49F5EC5A1D945CDF85E0E3379BB22EE9', secret='YGtnwArOdprZ906xl5uHhnF7pEF4q+egMj/mcgZ+')
-shinyapps::deployApp('.')
